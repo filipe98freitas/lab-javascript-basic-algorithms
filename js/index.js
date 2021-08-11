7b987a13d9d5968841672f22e841f53987810f99
filipe98freitas <filipe98freitas@gmail.com>
2021-08-11 21:52:56 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 // Iteration 1: Names and Input
+let hacker1"Filipe"
+let hacker2="Alexandre"
 
+console.log(`The drivers name is ${hacker1}`)
+console.log(`The navigators name is ${hacker2}`)
 
 // Iteration 2: Conditionals
+hacker1 = hacker1.toUpperCase()
+let outroHacker1 = ""
+for(let i=0; i<hacker1.length; i++){
+  outroHacker1 += hacker1[i] + " "
+}
+console.log(outroHacker1);
 
-
-// Iteration 3: Loops
+let outroHacker2 = ""
+for(let i=hacker2.length-1; i>=0; i--){
+  outroHacker2 += hacker2[i]
+}
+console.log(outroHacker2)
